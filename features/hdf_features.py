dc15112fde4c9e196e35e822025f168d5e0a1b5b
sijia66 <sijia66@uw.edu>
2020-10-02 12:01:50 -0700

    purge unnessary imports in hdf_features

--- features/hdf_features.py
+++ features/hdf_features.py
@@ -9,8 +9,6 @@ import numpy as np
 import fnmatch
 import os, sys
 import subprocess
-from riglib import calibrations, bmi
-from riglib.bmi import extractor
 from riglib.experiment import traits
 
 
